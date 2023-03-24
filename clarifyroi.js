6f32195483a4237ebc53c848d4596eef4e583163
modwrx <128759306+modwrx@users.noreply.github.com>
2023-03-24 09:11:26 -0400

    Update clarifyroi.js

--- clarifyroi.js
+++ clarifyroi.js
@@ -8,12 +8,58 @@ function createCookie(name, value, days) {
   document.cookie = name + "=" + value + expires + "; path=/";
 }
 
-createCookie("wc_visitor", "94387-44e84ff3-ae0f-d658-e5c4-bd8003ecf4a8", 365);
+const queryString = window.location.search;
+
+const urlParams = new URLSearchParams(queryString);
 
 
+const source = urlParams.get('utm_source');
+const medium = urlParams.get('utm_medium');
+const campaign = urlParams.get('utm_campaign');
+const keyword = urlParams.get('utm_keyword');
 
-var myCookieValue = readCookie("wc_visitor");
-console.log(myCookieValue);
+const currentUrl = window.location.href;
+
+var CROICookie = readCookie("croi_user");
+var wcCookie = readCookie("wc_visitor");
+if(wcCookie != null)
+{
+    if(CROICookie === null || CROICookie != wcCookie) 
+    {
+        const data = {
+            'source' : source,
+            'user_id' : wcCookie,
+            'medium' : medium,
+            'campaign' : campaign,
+            'keyword' : keyword,
+            'currentUrl' : currentUrl
+        };
+
+        createCookie('croi_user', wcCookie, .0005);
+        console.log('roi doesnt exist or is not equal');
+
+        const apiUrl = 'http://192.168.1.30/api/clarifyroi';
+
+        fetch(apiUrl, {
+            method: 'POST',
+            body: JSON.stringify(data),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+            .then(response => response.json())
+            .then(data => {
+            // Handle the API response data
+            console.log(data);
+            })
+            .catch(error => {
+            // Handle any errors that occur during the API request
+            console.error(error);
+        });
+    }
+}
+
+
 
 
 function readCookie(name) {
@@ -30,49 +76,3 @@ function readCookie(name) {
   }
   return null;
 }
-
-    // Retrieve the query string parameters from the current URL
-const queryString = window.location.search;
-
-// Parse the query string into an object
-const urlParams = new URLSearchParams(queryString);
-
-// Get the values of the source, medium, and campaign parameters
-const source = urlParams.get('utm_source');
-const medium = urlParams.get('utm_medium');
-const campaign = urlParams.get('utm_campaign');
-const keyword = urlParams.get('utm_keyword');
-
-// Do something with the captured values
-console.log('Source: ' + source);
-console.log('Medium: ' + medium);
-console.log('Campaign: ' + campaign);
-console.log('Keyword: ' + keyword);
-
-const data = {
-    'Source: ' : source,
-    'user_id' : myCookieValue
-};
-
-// Define the API endpoint URL
-const apiUrl = 'http://192.168.1.30/api/wctest';
-
-// Call the API endpoint when the page loads
-window.addEventListener('load', () => {
-  fetch(apiUrl, {
-    method: 'POST',
-    body: JSON.stringify(data),
-    headers: {
-        'Content-Type': 'application/json'
-    }
-  })
-    .then(response => response.json())
-    .then(data => {
-      // Handle the API response data
-      console.log(data);
-    })
-    .catch(error => {
-      // Handle any errors that occur during the API request
-      console.error(error);
-    });
-});
